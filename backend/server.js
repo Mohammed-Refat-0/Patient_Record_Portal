2631a0cfd78e1348a322ad0470394bbe653ac704
Mohammed Refat <mohammedrefat53@outlook.com>
2024-10-06 23:40:54 +0300

    initialize project

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+// Initiate the server
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.port || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
