de06299bde449f76f6deedcfde91385142c6a28e
Mohammed Refat <92163894+Mohammed-Refat-0@users.noreply.github.com>
2024-10-09 02:36:49 +0300

    DB setup

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+// connection script to mongodb atlas cluster
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+export default connectDB
