7e6988a57fea0d719bde55bc4451d5dfdee830e5
Mohammed Refat <92163894+Mohammed-Refat-0@users.noreply.github.com>
2024-10-12 05:58:05 +0300

    hcp and patient router

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ import connectDB from './config/db.js';
 connectDB();
 
 import adminRoutes from './routes/adminRoutes.js';
+import hcpRoutes from './routes/hcpRoutes.js';
+import patientRoutes from './routes/patientRoutes.js';
 
 const port = process.env.port || 5000;
 
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/admin', adminRoutes);
+app.use('/api/hcp', hcpRoutes);
+app.use('/api/patient', patientRoutes);
 app.get('/', (req, res) => res.send('API running'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
