cec28942c31369dbbd97bba55afd17564bb307a8
Mohammed Refat <92163894+Mohammed-Refat-0@users.noreply.github.com>
2024-10-28 08:28:28 +0300

    production

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 import connectDB from './config/db.js';
@@ -23,6 +24,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/admin', adminRoutes);
 app.use('/api/hcp', hcpRoutes);
 app.use('/api/patient', patientRoutes);
-app.get('/', (req, res) => res.send('API running'));
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
